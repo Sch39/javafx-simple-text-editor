05fb5a6e2056e72913d2c166eefd445a16c5ad30
sch39 <sukronraezpactoer@gmail.com>
2025-08-03 18:29:02 +0700

    add: style for sidebar component

--- src/main/java/dev/sch/simpletexteditor/ui/components/SidebarComponent.java
+++ src/main/java/dev/sch/simpletexteditor/ui/components/SidebarComponent.java
@@ -1,11 +1,13 @@
 package dev.sch.simpletexteditor.ui.components;
 
+import dev.sch.simpletexteditor.SimpleTextEditorApp;
 import javafx.geometry.Insets;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
 
 import java.io.File;
+import java.util.Objects;
 
 @Getter
 public class SidebarComponent extends VBox {
@@ -19,5 +21,8 @@ public class SidebarComponent extends VBox {
         this.getStyleClass().add("sidebar-component");
         this.setPadding(new Insets(10));
         this.getChildren().add(this.fileTreeView);
+
+        this.getStylesheets()
+                .add(Objects.requireNonNull(SimpleTextEditorApp.class.getResource("styles/component/sidebar-component.css")).toExternalForm());
     }
 }
