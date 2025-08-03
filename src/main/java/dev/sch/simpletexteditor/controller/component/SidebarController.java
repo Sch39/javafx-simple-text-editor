a1afb76f90c4b91bd472aa300d1c1f551b3fffc8
sch39 <sukronraezpactoer@gmail.com>
2025-08-03 20:58:42 +0700

    fix: sorted list directory content, with folder priority on top then file

--- src/main/java/dev/sch/simpletexteditor/controller/component/SidebarController.java
+++ src/main/java/dev/sch/simpletexteditor/controller/component/SidebarController.java
@@ -8,9 +8,10 @@ import javafx.scene.control.TreeItem;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
 
 public class SidebarController implements IController<SidebarComponent> {
     private final AppContext ctx;
@@ -50,8 +51,11 @@ public class SidebarController implements IController<SidebarComponent> {
         sidebarComponent.getFileTreeView().setShowRoot(true);
         rootItem.setExpanded(true);
 
-        try(DirectoryStream<Path> stream = Files.newDirectoryStream(dirPath)){
-            for (Path entry:stream){
+        try{
+            List<Path> sortedEntries = Files.list(dirPath)
+                    .sorted(new FileComparator())
+                    .toList();
+            for (Path entry:sortedEntries){
                 TreeItem<File> item = createTreeItem(entry);
                 rootItem.getChildren().add(item);
             }
@@ -73,8 +77,11 @@ public class SidebarController implements IController<SidebarComponent> {
                                 && item.getChildren().getFirst() != null
                                 && item.getChildren().getFirst().getValue() == null){
                             item.getChildren().removeFirst();
-                            try(DirectoryStream<Path> stream = Files.newDirectoryStream(path)){
-                                for (Path subEntry : stream){
+                            try{
+                                List<Path> sortedSubEntries = Files.list(path)
+                                        .sorted(new FileComparator())
+                                        .toList();
+                                for (Path subEntry : sortedSubEntries){
                                     item.getChildren()
                                             .add(createTreeItem(subEntry));
                                 }
@@ -86,5 +93,22 @@ public class SidebarController implements IController<SidebarComponent> {
                     });
         }
         return item;
+    }
+
+    private static class FileComparator implements Comparator<Path>{
+
+        @Override
+        public int compare(Path path1, Path path2) {
+            boolean isDir1 = Files.isDirectory(path1);
+            boolean isDir2 = Files.isDirectory(path2);
+
+            if (isDir1==isDir2){
+                return path1.getFileName().toString().compareToIgnoreCase(path2.getFileName().toString());
+            }
+            if (isDir1){
+                return -1;
+            }
+            return 1;
+        }
     }
 }
